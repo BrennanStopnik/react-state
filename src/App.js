21ec962b3c8fb8be666465ac6c32eb295f175c9e
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-10-05 12:47:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+const App = () => {
+	const [firstName, setFirstName] = useState("Brennan")
+	const [age, setAge] = useState(0)
+
+
+	return (
+		<div className="App">
+			<p>{firstName}</p>
+			<p>{age}</p>
+			<input type="text" onChange={(event)=>{
+				const value = event.target.value
+				setFirstName(value)
+			}}/>
+			
+			<button onClick={()=>{
+				setAge(age + 1)
+			}}>Set Age</button>
+		</div>
+	);
 }
 
 export default App;
